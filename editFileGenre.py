bfb99056e86a02e894c22dd8ef63d1e4a054d5ec
Milosz Szymanski <milosz.jan.szymanski@gmail.com>
2024-02-01 14:16:48 +0100

    added ,,for,, that walk through all xls cells and extract genres

--- editFileGenre.py
+++ editFileGenre.py
@@ -11,7 +11,21 @@ start_value_a = 1
 
 modified_rows = []
 
+for row_idx, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+    value_c = row[2]
 
+    if isinstance(value_c, str):
+        value_c = value_c.strip()
+
+    if ',' in str(value_c):
+        split_values = str(value_c).split(',')
+
+        for split_value in split_values:
+            modified_rows.append([start_value_a, row[1], split_value.strip()])
+            start_value_a += 1
+    else:
+        modified_rows.append([start_value_a, row[1], value_c.strip()])
+        start_value_a += 1
 
 
 modified_sheet = workbook.create_sheet(title='Genres')
